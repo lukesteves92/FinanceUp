20016207eafd4d6a9f54f59f9bfc21f946ac5e65
lucas.esteves <lucas.coelho9280@gmail.com>
2024-11-22 12:52:10 -0300

    Configuring Gradle & Manifest Files

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,12 +1,14 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 kotlin {
@@ -29,20 +31,54 @@ kotlin {
     }
     
     sourceSets {
-        
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+
+            // Koin - Inject dependency
+            implementation(libs.koin.android)
+
+            // Ktor - Http Request
+            implementation(libs.ktor.client.android)
+            implementation(libs.ktor.client.okhttp)
+
+            // Splash Screen
+            implementation(libs.androidx.core.splashscreen)
+
+            // DataStore
+            implementation(libs.androidx.datastore.preferences)
         }
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
+            implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.runtime.compose)
+
+            // Navigation
+            implementation(libs.compose.navigation)
+
+            // Coil
+            implementation(libs.landscapist.coil3)
+
+            // Koin - Inject dependency
+            implementation(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+
+            // Ktor - Http Request
+            implementation(libs.bundles.ktor)
+
+            //Paging3
+            implementation(libs.paging.compose.common)
+            implementation(libs.paging.common)
+        }
+        iosMain.dependencies {
+            // Ktor - Http Request
+            implementation(libs.ktor.client.darwin)
         }
     }
 }
@@ -50,6 +86,10 @@ kotlin {
 android {
     namespace = "com.inspirecoding.financeup"
     compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
+    sourceSets["main"].res.srcDirs("src/commonMain/resources", "src/androidMain/res")
+    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
 
     defaultConfig {
         applicationId = "com.inspirecoding.financeup"
@@ -60,17 +100,45 @@ android {
     }
     packaging {
         resources {
+            excludes += "/META-INF/**"
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            merges += "META-INF/LICENSE.md"
+            merges += "META-INF/LICENSE-notice.md"
         }
     }
     buildTypes {
         getByName("release") {
+            applicationIdSuffix = ".prod"
+            versionNameSuffix = ".prod"
+            isMinifyEnabled =  true
+            isDebuggable = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            buildConfigField("String", "BASE_URL", "\"API_URL\"")
+        }
+        getByName("debug") {
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = ".dev"
             isMinifyEnabled = false
+            isDebuggable = true
+            buildConfigField("String", "BASE_URL", "\"API_URL\"")
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    tasks.withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+            freeCompilerArgs.add("-opt-in=kotlin.RequiresOptIn")
+        }
+    }
+    buildFeatures {
+        buildConfig = true
+        compose = true
     }
 }
 
